195dbfba571a252bfe20c0e3593e5e2085d7ffdd
junghwan-noh <tngur9408@gmail.com>
2026-05-20 16:25:02 +0900

    메인 페이지를 회사소개서로 리다이렉트

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,5 @@
-import HeroSection from '@/components/sections/HeroSection'
-import ConsultingCTA from '@/components/sections/ConsultingCTA'
-import MarketerProblemsSection from '@/components/sections/MarketerProblemsSection'
-import ProcessSection from '@/components/sections/ProcessSection'
-import WhyResonanceSection from '@/components/sections/WhyResonanceSection'
-import RealResultsSection from '@/components/sections/RealResultsSection'
-import StatsSection from '@/components/sections/StatsSection'
-import CampaignsSection from '@/components/sections/CampaignsSection'
-import InfluencerCardsSection from '@/components/sections/InfluencerCardsSection'
-import InsightsSection from '@/components/sections/InsightsSection'
-import ContactForm from '@/components/sections/ContactForm'
-import CompanyStoryCTA from '@/components/sections/CompanyStoryCTA'
-import FooterSection from '@/components/sections/FooterSection'
-import FloatingChatButton from '@/components/FloatingChatButton'
-import ScrollToTop from '@/components/ui/ScrollToTop'
-import Navbar from '@/components/ui/Navbar'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-black">
-      <Navbar />
-      <HeroSection />
-      <ConsultingCTA />
-      <ProcessSection />
-      <MarketerProblemsSection />
-      <WhyResonanceSection />
-      <RealResultsSection />
-      <StatsSection />
-      <CampaignsSection />
-      <InfluencerCardsSection />
-      <InsightsSection />
-      <ContactForm />
-      <CompanyStoryCTA />
-      <FooterSection />
-      <FloatingChatButton />
-      <ScrollToTop />
-    </main>
-  )
+  redirect('/company-profile.html')
 }

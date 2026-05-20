32b0c6f6d0675ecc257cca817c3e32ec78e15a03
junghwan-noh <tngur9408@gmail.com>
2026-05-20 16:30:08 +0900

    홈페이지 복구: 루트는 기존 Next.js 페이지로 유지

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return {
-      beforeFiles: [
-        { source: '/', destination: '/company-profile.html' },
-      ],
-    };
-  },
 }
 
 module.exports = nextConfig
